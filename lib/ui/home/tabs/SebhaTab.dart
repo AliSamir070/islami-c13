ee2b3b7df2ee6e9ffdc19e923cdb2f8e693a4f02
Ali070 <Ali.zidan39@yahoo.com>
2025-01-14 00:36:51 +0200

    + finish sebha and onboarding

--- lib/ui/home/tabs/SebhaTab.dart
+++ lib/ui/home/tabs/SebhaTab.dart
@@ -4,7 +4,7 @@ import '../../../style/AssetsManager.dart';
 
 class SebhaTab extends StatelessWidget {
   const SebhaTab({super.key});
-
+  // add sebha image and rotation
   @override
   Widget build(BuildContext context) {
     return Container(
